0f20542bd57a63a2946c199cc113bf4b15de8252
Zhantao Deng @EPFL <zhantao.deng@epfl.ch>
2019-04-24 15:42:10 +0200

    added a function to convert depthmap to radial depthmap so that we can calculate the errors

--- pyscript/Omni_DepthEstimation.py
+++ pyscript/Omni_DepthEstimation.py
@@ -1,3 +1,6 @@
 from colmaplib import create_workspace
 
-create_workspace(image_dir='../../../dataset/omnidirectional/1024_512/', file_suffix='png', work_dir = '../data')
+create_workspace(image_dir='../../../dataset/omnidirectional/1024_512/', file_suffix='exr', work_dir = '../data', 
+                 is_depth=True)
+
+#create_workspace(image_dir='../../../dataset/omnidirectional/1024_512/', file_suffix='png', work_dir = '../data')
--- pyscript/colmaplib.py
+++ pyscript/colmaplib.py
@@ -14,7 +14,8 @@ import numpy as np
 from cam360 import Cam360
 from cubicmaps import CubicMaps
 
-def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='./'):
+def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='./', 
+                     is_depth: bool = False, radius_depth: bool = False, cam_para: list = None):
         """
             It projects all omnidirectional images under the given directory to cubic maps. 
             Then it collects cubic maps according to the view (back/front/laft/right) and 
@@ -23,13 +24,24 @@ def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='.
             Parameters
             ----------    
             image_dir : str
-                Path to the omnidirectional images
+                Path to the omnidirectional images;
                 
             file_suffix : str
-                The format of omnidirectional images 
+                The format of omnidirectional images;
                 
             work_dir :
-                Where to save cubic images.
+                Where to save cubic images;
+                
+            is_depth: bool
+                Whether the images to be load are depthmaps (.exr file);
+                
+            radius_depth: bool
+                Whether to save radius depthmap or the euclidean depth;
+                only used for comparing the estimated depth map with the ground truth;  
+            
+            cam_para: list
+                List of camera parameters, containing 3 parameters: 
+                [focal length x, focal length y, camera center on rows, camera center on columns]
     
             Returns
             -------
@@ -42,12 +54,15 @@ def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='.
         """
         if len(file_suffix) <= 0:
                 raise ValueError("Input ERROR! Invalid file suffix") 
-                
-        try:    
-            cubemap_obj = CubicMaps(expand_fov=1.0)
+                   
+        cubemap_obj = CubicMaps()
+        
+        file_pattern = '*.'+file_suffix
+        for filename in glob.glob(os.path.join(image_dir, file_pattern)):
             
-            file_pattern = '*.'+file_suffix
-            for filename in glob.glob(os.path.join(image_dir, file_pattern)):
+            prefix = filename.split(sep='/')[-1][:-4]
+            
+            if is_depth==False:
                 # load omnidirectional images
                 Omni_img = np.flip(cv2.imread(filename), axis=2)   
                 Omni_img = Omni_img/np.max(Omni_img)
@@ -55,9 +70,7 @@ def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='.
                 Omni_obj = Cam360(rotation_mtx = np.eye(3), translation_vec=np.zeros([3,1]), 
                                   height = Omni_img.shape[0], width = Omni_img.shape[1], channels = Omni_img.shape[2], 
                                   texture= Omni_img)
-                
-                prefix = filename.split(sep='/')[-1][:-4]
-                
+                                
                 cubemap_obj.sphere2cube(Omni_obj, resolution=(512,512))
                 
                 for ind in range(len(cubemap_obj.cubemap)):
@@ -66,12 +79,30 @@ def create_workspace(image_dir: str='', file_suffix: str='png', work_dir: str='.
                         os.makedirs(directory)
                         
                     cubemap_obj.save_cubemap(path = directory, prefix = prefix, index=[ind])     
+                
+            else:
+                # load omnidirectional depthmap
+                Omni_dep = cv2.imread(filename, cv2.IMREAD_ANYDEPTH)
+                
+                Omni_obj = Cam360(rotation_mtx = np.eye(3), translation_vec=np.zeros([3,1]), 
+                                  height = Omni_dep.shape[0], width = Omni_dep.shape[1], channels = 1, 
+                                  depth = Omni_dep)
+            
+                cubemap_obj.sphere2cube(Omni_obj, resolution=(512,512), is_depth=is_depth)
                     
-        except:
-            raise ValueError("Input ERROR! Invalid path")
-#           
-#def create_camera_model( intrinsic_para: list=[], num_camera: int = 2, same_camera: bool = True):
-#    if 
+                for ind in range(len(cubemap_obj.depthmap)):
+                    directory = work_dir + '/view' + str(ind) + '/'
+                    if not os.path.exists(directory):
+                        os.makedirs(directory)
+                        
+                    if radius_depth:
+                        cubemap_obj.save_cubedepth(path = directory, prefix = prefix, index=[ind], 
+                                                   dist_to_radius=True, camera_para=cam_para)     
+                    else:
+                        cubemap_obj.save_cubedepth(path = directory, prefix = prefix, index=[ind])     
+           
+def create_camera_model( intrinsic_para: list=[], num_camera: int = 2, same_camera: bool = True):
+    if 
 #    
 #def create_imagedb():
         
